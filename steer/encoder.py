ee8940a550d4a25a55d9261714238768f99c2666
starcolon <tao@starcolon.com>
2020-04-03 00:47:43 +0200

    debug

--- steer/encoder.py
+++ steer/encoder.py
@@ -19,15 +19,21 @@ class StateActionEncoder:
 
 class PixelateStateActionEncoder(StateActionEncoder):
 
+  def __init__(self):
+    self.n = 0
+
   def encode_state(self, s):
     """
     Encode 96x96x3 observation into 8x8
     """
     b8x8 = cv2.resize(s, (8,8))
 
+    # DEBUG
+    cv2.imwrite("debug/{}.png".format(self.n), b8x8)
+    self.n = (self.n+1)%100
+
     for y in range(8):
       for x in range(8):
         b8x8[y,x] = b8x8[y,x]//32
-
     return np.array2string(b8x8, precision=0)
 
