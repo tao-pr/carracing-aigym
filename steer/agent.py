789417d4713b3f704c5338eaa536221b7fdb3a0d
starcolon <tao@starcolon.com>
2020-04-02 23:12:41 +0200

    added initial learn script

--- steer/agent.py
+++ steer/agent.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import joblib
 
@@ -6,24 +7,24 @@ class Agent:
   Base reinforcement learning agent with basic interface
   """
   def __init__(self, learning_rate=0.25):
-    self.alpha = learning_rate
+    self.learning_rate = learning_rate
 
     # Two-level dictionaries
     self.policy = dict() # [state => action => reward]
     self.state_machine = dict() # [state => state' => action]
+
+    # Binding state and action encoder (np.array => str)
+    self.encode_state = lambda s: np.array2string(s, precision=0) # Dummy one, should be overridden
+    self.encode_action = lambda s: np.array2string(s)
+
+    # Decode action hash into vector (str => np.array)
+    self.decode_action = lambda s: np.fromstring(s)
 
   def reset(self):
     """
     Reset all internal states
     """
     pass
-
-  def encode_state(self, state) -> str:
-    """
-    Encode `state` (np.array) as observed from the environment 
-    into a hashable string
-    """
-    return ""
 
   def learn(self, state, action, reward, next_state):
     """
@@ -32,23 +33,33 @@ class Agent:
     - we will get back the reward (double value)
     - and register the next state (np.array)
     """
-    old_v = self.get_v(state, action)
-    new_v = self.get_v(next_state, action)
-
-    # Update policy
-    self.policy[]
-
-    # Update state machine
-    # TAOTODO
+    pass
 
   def best_action(self, state):
-    return 0
+    """
+    Return the best action to take on the specified state 
+    to maximise the possible reward
+    """
+    statehash = self.encode_state(state)
+    if statehash not in self.state_machine:
+      # Unrecognised state, return no recommended action
+      return (-1, 0)
+    best_action = -1
+    best_reward = 0
+    for next_statehash in self.state_machine[statehash]:
+      a = self.state_machine[statehash][next_statehash]
+      v = self.get_v(statehash, a)
+      if v >= best_reward:
+        best_reward = v
+        best_action = a
+    return (best_action, best_reward)
 
   def get_v(self, state, action):
     """
     Evaluate the reward value of `action` taken on the `state`
     """
     statehash = self.encode_state(state)
+    actionhash = self.encode_action(action)
     if statehash not in self.policy:
       self.policy[statehash] = {action: 0}
       return 0
@@ -64,7 +75,44 @@ class Agent:
       joblib.dump(self, f, compress=1)
 
   @staticmethod
-  def load(path):
+  def load(path, default):
     if os.path.isfile(path):
       with open(path, "rb") as f:
+        print("Agent loaded from {}".format(path))
         return joblib.load(f)
+    else:
+      print("No agent file to load, created a new one")
+      return default
+
+
+class TDAgent(Agent):
+  """
+  Temporal difference
+  """
+  def __init__(self, learning_rate=0.25, alpha=0.9):
+    self.alpha = alpha
+    super().__init__(learning_rate)
+
+  def learn(self, state, action, reward, next_state):
+    statehash = self.encode_state(state)
+    newstatehash = self.encode_state(next_state)
+    
+    old_v = self.get_v(state, action)
+    new_v = self.get_v(next_state, action)
+
+    # Update policy (weighted temporal difference)
+    diff = self.learning_rate * (reward + self.alpha * new_v - old_v)
+    self.policy[statehash][action] = old_v + diff
+
+
+class QAgent(Agent):
+  """
+  Simple Q-learning
+  """
+  pass
+
+class PGAgent(Agent):
+  """
+  Policy Gradient
+  """
+  pass
---
+++ steer/learn.py
@@ -0,0 +1,50 @@
+import gym
+
+from .agent import Agent, TDAgent
+
+if __name__ == '__main__':
+
+  # Hardcoded settings
+  n_episodes = 1
+  path       = "tdagent.bin"
+
+  # Create an env, load or create an agent
+  env   = gym.make('CarRacing-v0')
+  agent = Agent.load(path, TDAgent(learning_rate=0.5, alpha=0.9))
+
+  # Start!
+  print("Starting the learning episodes")
+  for i in range(n_episodes):
+    
+    observation = env.reset()
+    print("Episode {} of {} ...".format(i+1, n_episodes))
+    
+    n = 0
+    while True:
+      n = n+1
+      env.render()
+
+      # Given the current state, ask the agent to find the best action to take
+      action = agent.best_action(observation)
+
+      # If the bot does not know how to react,
+      # random from the action space
+      if action == -1:
+        print("... Turn #{}, learning new action".format(n))
+        action = env.action_space.sample()
+      else:
+        print("... Turn #{}, taking action from experience".format(n))
+        action = agent.decode_action(action)
+
+      new_observation, reward, done, info = env.step(action)
+
+      # Learn
+      agent.learn(observation, action, reward, new_observation)
+
+      if done:
+        print("... DONE!")
+        # TODO reset the agent for the next episode
+        break
+
+  # Save the trained agent
+  # agent.save(path)
